94d5d88af7ac923182868ab046a43771bdc70368
stephanielees <timelytimeseries@gmail.com>
2025-03-16 16:06:29 +0000

    re-include the start pred_date in data_loader.py (setting it back to the original setting)

--- data/data_loader.py
+++ data/data_loader.py
@@ -343,7 +343,7 @@ class Dataset_Pred(Dataset):
             
         tmp_stamp = df_raw[['date']][border1:border2]
         tmp_stamp['date'] = pd.to_datetime(tmp_stamp.date)
-        pred_dates = pd.date_range(tmp_stamp.date.values[-1], periods=self.pred_len+1, freq=self.freq, inclusive='right')
+        pred_dates = pd.date_range(tmp_stamp.date.values[-1], periods=self.pred_len+1, freq=self.freq)
         
         df_stamp = pd.DataFrame(columns = ['date'])
         df_stamp.date = list(tmp_stamp.date.values) + list(pred_dates[1:])
